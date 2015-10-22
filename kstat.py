02a6c93f5c4518d90ba2ccdac7f56cf530626b6c
Mike Zeller <mike.zeller@joyent.com>
2015-10-22 10:44:39 -0700

    Take into account recommendations from PR-2

--- kstat.py
+++ kstat.py
@@ -4,13 +4,24 @@ log = logging.getLogger(__name__)
 LX_KSTAT = '/native/usr/bin/kstat'
 KSTAT = 'kstat'
 
+def _kstat(cmd, lx=False):
+    kstat = LX_KSTAT if lx else KSTAT
+    cmd = [ kstat, cmd]
+    return __salt__['cmd.run'](' '.join(cmd))
+
 def _process_kstat(kstat, cout, digitOnly):
     data = {}
     for line in cout.splitlines():
         key, value = line.split(":")[-1].split()
         if digitOnly:
-            if value.isdigit():
-                data[key] = value
+            v = value
+            try:
+                v = float(value)
+                v = long(value)
+            except:
+                pass
+            if isinstance(v, long) or isinstance(v, float):
+                data[key] = v
         else:
             data[key] = value
     return data
@@ -18,18 +29,10 @@ def _process_kstat(kstat, cout, digitOnly):
 
 
 def memory_cap(lx=False, digitOnly=False):
-    if lx:
-        cmd = [ LX_KSTAT, '-p -m memory_cap -c zone_memory_cap']
-    else:
-        cmd = [ KSTAT, '-p -m memory_cap -c zone_memory_cap']
-    output = __salt__['cmd.run'](' '.join(cmd))
+    output = _kstat('-p -m memory_cap -c zone_memory_cap', lx)
     return _process_kstat('memory_cap', output, digitOnly)
 
 
 def zone_zfs(lx=False, digitOnly=False):
-    if lx:
-        cmd = [ LX_KSTAT, '-p -m zone_vfs -c zone_vfs']
-    else:
-        cmd = [ KSTAT, '-p -m zone_vfs -c zone_vfs']
-    output = __salt__['cmd.run'](' '.join(cmd))
+    output = _kstat('-p -m zone_vfs -c zone_vfs', lx)
     return _process_kstat('memory_cap', output, digitOnly)
